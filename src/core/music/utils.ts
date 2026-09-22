a274da8a97b2489fa97ea93508b7ffaa53e486f1
wangtf <wangtf@seclead.cn>
2026-09-22 12:14:54 +0800

    style: 移除上游 v1.9.1 引入的冗余 any 断言

--- src/core/music/utils.ts
+++ src/core/music/utils.ts
@@ -442,7 +442,7 @@ export const getOnlineOtherSourceLyricInfo = async({ musicInfos, onToggleSource,
   let reqPromise
   try {
     // TODO: remove any type
-    reqPromise = (musicSdk[musicInfo.source].getLyric(toOldMusicInfo(musicInfo)) as any).promise
+    reqPromise = (musicSdk[musicInfo.source].getLyric(toOldMusicInfo(musicInfo))).promise
   } catch (err: any) {
     reqPromise = Promise.reject(err)
   }
@@ -477,7 +477,7 @@ export const handleGetOnlineLyricInfo = async({ musicInfo, onToggleSource, isRef
   let reqPromise
   try {
     // TODO: remove any type
-    reqPromise = (musicSdk[musicInfo.source].getLyric(toOldMusicInfo(musicInfo)) as any).promise
+    reqPromise = (musicSdk[musicInfo.source].getLyric(toOldMusicInfo(musicInfo))).promise
   } catch (err) {
     reqPromise = Promise.reject(err)
   }
